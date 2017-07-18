ea548bd64838779070006081a182067a0dc5ed9d
Admin <admin@Admins-MacBook-Pro.local>
2017-07-18 16:05:24 -0700

    first commit

---
+++ projects/Pre-course/app.js
@@ -0,0 +1,25 @@
+//var main = function () {
+// $(".dropdown-toggle").click(function() {
+//   $ (".dropdown-menu").toggle();
+//  });
+//};
+// $(document).ready(main);
+//var hold;
+//console.log(hold);
+//console.log("in app.js file");
+/* var hold = {
+  ordID: 2093,
+  isActive: true
+};
+console.log(typeof hold); */
+//var OrdID = 9003;
+//  console.log(OrdID)
+
+//function printOrder(OrdID){           //function declaration, global
+//  console.log('Printing order. ' + OrdID);
+//};
+//printOrder();
+
+function calculateTotalPrice(quantity, price){      
+  console.log('Total price: ' + (quantity));
+};
---
+++ warmups/helloClass/app.js
@@ -0,0 +1,8 @@
+function hi(hello, world) {
+   return hello + world;
+}
+
+hi("Hello, " "Class!")
+//   function hello (val1, val2) {
+//     return val1 + val2 ;
+//   }
