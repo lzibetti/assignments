8f8507c7685f33c3a71c07ccc529b7ab58415f90
lzibetti <lzibetti@comcast.net>
2017-07-20 12:25:33 -0700

    loops and arrays

---
+++ warmups/boxModel/main.js
@@ -0,0 +1,133 @@
+//var isEmpty = true;
+//var isThisEmpty = false;
+//var count = 0;
+//
+//function add(num1, num2){
+//  return "abcde"
+//}
+//
+//if (1) {
+//  abcde
+//  //add(2,2)
+//  console.log("true")
+//    } else {
+//      console.log("false")
+//    }
+//
+
+//var isEmpty = true;
+//var isThisEmpty = false;
+//var a = 90;
+//
+//function add(num1, num2){
+//  return num1 + num2
+//}
+//  
+//if (a <= 18 || true) {
+//  console.log(add(2,2));
+//  console.log(true);
+//} else {
+//  console.log("false");
+//}
+
+//var isEmpty = true;
+//var isThisEmpty = false;
+//var a = 90;
+//var myArr = [1, 2, 3]
+//
+//function add(num1, num2){
+//  return num1 + num2
+//}
+//  
+//if (myArr.length) {
+//  console.log(add(2,2));
+//  console.log(true);
+//} else {
+//  console.log("false");
+//}
+
+//else if
+//var isEmpty = true;
+//var isThisEmpty = false;
+//var a = 99;
+//var myArr = []
+//
+//if (a === 10) {
+//  console.log(true);
+//} else if (a === 99) {
+//  console.log('else if')
+//} else {
+//  console.log("false")
+//}
+
+// ternary block
+
+// evaluate true: false  
+//var x = () ? {} : false
+
+//switch
+//switch (a) {
+//  case '12':
+//      console.log(12);
+//      break;
+//  case 99:
+//      console.log('num');
+//      break;
+//  case '99':
+//      console.log('99');
+//      break;
+//}
+
+////////////////////////////////////////
+
+// while loop
+//var num = 0
+//while(num <= 100) {
+//  console.log(num);
+//  num++
+//}
+
+// do loop
+//do {
+//  console.log(num)
+//  num++;
+//}while (num < 100)
+
+// for loop
+//var num = 1;
+var myArr = ["Jess", "AJ", "Travis"]
+
+for (var i=0; i < myArr.length; i++) {
+  if (myArr[i] === "Travis")
+      console.log(i)
+      console.log(myArr[i])
+
+//  num++
+//  if (num > 10) break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
