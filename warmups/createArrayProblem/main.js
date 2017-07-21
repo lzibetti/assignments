16620251c12277add9a1e7dea26115f8f6bcbb7e
lzibetti <lzibetti@comcast.net>
2017-07-21 14:23:02 -0700

    css practice

---
+++ warmups/createArrayProblem/main.js
@@ -0,0 +1,50 @@
+//var fruits = ['banana', 'starfruit', 'kumquat', 'watermelon', 'grape', 'apple', 'orange'];
+
+
+
+// function calling a function
+function farmArray(){
+    var farmThings[]
+    letsSpliceFarm(farmThings)
+    function letsSpliceFarm(imps){
+//        ```code block ```
+    }
+}
+
+
+// 
+//function eat(item, numOfUser, favoriteFood){
+//  if (favoriteFood === "pie") {
+//    return sum(item, 1)
+//  }
+//  else {
+//    return "not 3"
+//  }
+//}
+//
+//function sum(num1, num2, favoriteFood){
+//  var total = 0;
+//  total = num1 + num2;
+//  return total;
+//}
+
+
+function arr() {
+    var arr1 = []
+    doSplice(arr1)
+    function doSlice(parm) {
+      
+    }
+    function doSplice() {
+      //run slice
+      // newArr = sliceStuff
+      //console.log(newArr)
+      //console.log(addToArray(newArr))
+    }
+}
+function addToArray(arr) {
+  //push 999 into arr and return it
+}
+function whatever2() {
+  
+}
